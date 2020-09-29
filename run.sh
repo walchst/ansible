523d9f0e6196e241ac2cdf2f7c53dbd55736b024
Steven Walch <walchst@gmail.com>
2020-09-29 20:47:53 +1000

    testing

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 echo Hello World!
 apt install -y ansible git                                                                                                                           
 ansible-galaxy collection install community.general                                                                                                       
-ansible-playbook local.yml -K                      
+#ansible-playbook local.yml -K                      
+ansible-pull --url https://github.com/walchst/ansible.git -K
