52f5b65c34bd403b9b8160bde89ecf9ce578dd94
Steven Walch <walchst@gmail.com>
2021-04-13 17:43:38 +1000

    first attempt to integrate gnome-extensions

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 echo Hello World!
 apt install -y ansible git                                                                                                                           
 ansible-galaxy collection install community.general                                                                                                       
-#ansible-playbook local.yml -K                      
-ansible-pull --url https://github.com/walchst/ansible.git -K
+#ansible-galaxy install luizgavalda.gnome_extensions
+#ansible-galaxy install jaredhocutt.gnome_extensions
+ansible-playbook local.yml -K                      
+#ansible-pull --url https://github.com/walchst/ansible.git -K
